8605a9e8cac9e41171f0f8ea3149926a6712773c
byron mccormick <byronjamesmccormick@gmail.com>
2024-08-08 15:46:39 +1200

    add new project

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -2,8 +2,21 @@
 export const projectsData = [
     {
         "id": 1,
+        "name": "āeCHEF",
+        "description": "āeCHEF is a robust, full-scale enterprise-level web application and my biggest project yet! Leveraging modern technologies such as TypeScript, MongoDB, and React, this platform allows users to create and manage restaurants. Key features include intuitive menu creation, efficient order management, and secure payment processing through Stripe integration. With Auth0 integration, users benefit from seamless authentication, while ShadCN and Tailwind ensure a sleek, responsive UI. Advanced search and filter capabilities enable users to find and order from their preferred restaurants easily. The integration with Cloudinary facilitates optimized image handling and storage. Working on āeCHEF was an incredible journey, offering deep insights into enterprise-level application development.",
+        "tools": ["TypeScript", "MongoDB", "React", "Stripe", "Auth0", "ShadCN", "Tailwind", "Cloudinary"],
+        "role": "Full Stack Developer",
+        "code": "",
+        "demo": "",
+        "image": "",
+        "githubLink": "https://github.com/byronmccormick/aechef-backend",
+        "liveLink": "https://aechef-frontend.onrender.com/"
+    }
+    ,
+    {
+        "id": 2,
         "name": "SurfCircle",
-        "description": "SurfCircle is a dynamic single-page application designed to track surf sessions and verify the accuracy of surf reports. Built with the MERN stack, it features user authentication, state management with Redux, and a sleek, responsive UI. Users can create posts, comment, like, and search for posts within the community. The project includes a comprehensive dashboard for keeping track of you surf stats. I learned a massive amount while building SurfCircle and over all, it was an amazing experience. I can't wait to get started on my next idea!",
+        "description": "SurfCircle is a dynamic single-page application designed to track surf sessions and verify the accuracy of surf reports. Built with the MERN stack, it features user authentication, state management with Redux, and a sleek, responsive UI. Users can create posts, comment, like, and search for posts within the community. The project includes a comprehensive dashboard for keeping track of you surf stats. I learned a massive amount while building SurfCircle and over all, it was an amazing experience.",
         "tools": ["MongoDB", "Express", "React", "Node.js", "Firebase", "Redux"],
         "role": "Full Stack Developer",
         "code": "",
@@ -14,7 +27,7 @@ export const projectsData = [
     }
     ,
     {
-        "id": 2,
+        "id": 3,
         "name": "AbodeBooking Setup Wizard",
         "description": "The AbodeBooking Setup Wizard is a professional, customer-facing application designed to streamline the process of creating new property accounts for AbodeBooking. Initially a small internal app, it was transformed into a user-friendly tool that allows property owners to input essential information and property details such as rooms, pricing, and more. This setup wizard significantly enhances the user experience by simplifying the onboarding process for new properties into the AbodeBooking property management system.",
         "tools": ["PHP", "MySQL", "Bootstrap 5", "Javascript", "Jquery"],
@@ -25,7 +38,7 @@ export const projectsData = [
     }
     ,
     {
-        "id": 3,
+        "id": 4,
         "name": "Saphire Salon",
         "description": "Saphire Salon is a beautiful static website designed for a hair salon, featuring a fully responsive design and mobile-style menu. This elegant site allows users to get in contact via email submissions. Built using HTML5, CSS3 with flexbox, Sass, JavaScript, and jQuery, the site includes pre-styled elements for easy customization and utilizes Sass mixins and functions for efficient styling.",
         "tools": ["HTML5", "CSS3", "Sass", "JavaScript", "jQuery"],
@@ -35,7 +48,7 @@ export const projectsData = [
         "image": ""
     },
     {
-        "id": 4,
+        "id": 5,
         "name": "Land-Ho!",
         "description": "Land-Ho! is a stunning single-page, single-screen responsive landing page designed to direct users to your online presence. The site makes heavy use of CSS animations, creating a dynamic and engaging user experience. The scrolling mountainous background, inspired by Ryan Schroeder's 'Icefields', adds a captivating visual element. This simple yet visually appealing template is perfect for showcasing your content and directing visitors to your other online platforms.",
         "tools": ["HTML5", "CSS3", "Sass", "JavaScript", "jQuery"],
