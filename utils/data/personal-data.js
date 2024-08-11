c11cd4c04d7ea4ddd1a0c4cee557cc2bbb697c78
Byron McCormick <byronjamesmccormick@gmail.com>
2024-08-12 07:16:47 +1200

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "BYRON",
   profile: '/profile.jpg',
-  designation: "Problem Solver",
+  designation: "Software Engineer",
   descriptionLine1: "👋 Hey there!   I'm Byron, a software engineer who thrives on tackling complex problems and building elegant solutions.  While my background includes adventures in outdoor education and working with special needs children, these days I'm all about crafting clean, efficient code at AbodeBooking.  I'm a passionate learner and constantly seeking ways to improve my skills. I am currently seeking new opportunities to leverage my skills and make a meaningful impact in the tech industry.",
   descriptionLine2: "When I'm not coding, you can find me ski touring the North Islands volcanos, catching waves at my local beach, or rock climbing. I'm a huge believer in lifelong learning, so I'm always keen to try something new and push my limits.",
   email: 'byronjamesmccormick@gmail.com',
